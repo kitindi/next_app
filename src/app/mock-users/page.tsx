3e1f7017599723058a8056e567459a46f5b817fd
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-01-14 18:06:29 +0300

    Added Server Actions for updating data in the database securely

--- src/app/mock-users/page.tsx
+++ src/app/mock-users/page.tsx
@@ -1,3 +1,4 @@
+import { revalidatePath } from "next/cache";
 type MockUser = {
   id: number;
   name: string;
@@ -7,13 +8,39 @@ export default async function MockUsers() {
   const res = await fetch("https://6786774af80b78923aa6f058.mockapi.io/users");
   const users = await res.json();
 
+  //   server actions
+
+  async function addUser(formData: FormData) {
+    "use server";
+    const name = formData.get("name");
+    const res = await fetch("https://6786774af80b78923aa6f058.mockapi.io/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json", Authorization: "Baerer YOUR_PRIVATE_KEY" },
+      body: JSON.stringify({ name }),
+    });
+
+    const newUser = await res.json();
+    revalidatePath("/mock-users");
+    console.log(newUser);
+  }
+
   return (
-    <ul className="w-full max-w-3xl mx-auto mt-5 p-8 bg-white flex flex-col space-y-4">
-      {users.map((user: MockUser) => (
-        <li key={user.id} className="bg-slate-300 p-4 rounded-md">
-          {user.name}
-        </li>
-      ))}
-    </ul>
+    <div className="w-full max-w-2xl mx-auto">
+      <div className="py-10 w-92">
+        <form action={addUser}>
+          <div className="mb-3">
+            <input type="text" name="name" required className="border border-gray-500 px-3 py-2 focus:outline-none w-92" />
+          </div>
+          <button className="bg-black text-white px-12 py-2 rounded-sm">Add new user</button>
+        </form>
+      </div>
+      <ul className="w-full max-w-3xl mx-auto mt-5 p-8 bg-white flex flex-col space-y-4">
+        {users.map((user: MockUser) => (
+          <li key={user.id} className="bg-slate-300 p-4 rounded-md">
+            {user.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
